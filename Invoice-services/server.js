f6c9762245aaa2ce9650bce74a92800404857a54
sandesh-08 <sandesh.btech.ec19@iiitranchi.ac.in>
2021-11-07 16:01:32 +0530

    connecting local database and general API's

--- Invoice-services/server.js
+++ Invoice-services/server.js
@@ -1,12 +1,22 @@
-const express= require('express');
-
+const express = require("express");
+const mongoose = require("mongoose");
 const app=express();
+const invoiceRoutes = require("./routes/Invoice");
+const PORT = process.env.PORT || 5000;
+MONGO_URL = "mongodb://127.0.0.1:27017/InvoiveLocalDataBase"
 
+app.use(express.json());
 
-app.get('/',(req,res)=>{
-    res.send("Invoice Application");
+mongoose.connect(MONGO_URL,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(()=>{
+    console.log("MONGODB Connected!!!!");
+}).catch((err)=>console.log("error with mongodb ",err));
+
+app.get("/",(req,res)=>{
+    res.send(` running `);
 })
 
-app.listen(3000,()=>{
-    console.log(`server is running on http://localhost:${3000}`)
-})
+app.use("/Invoice", invoiceRoutes);
+app.listen(PORT,()=>console.log("backend server is running ",PORT));
